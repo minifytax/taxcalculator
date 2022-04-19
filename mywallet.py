49079479c36f2f57d7a16534d7c29ccf0dab13f9
Tehreem <tehreem@minifytax.com>
2022-04-19 07:53:30 -0700

    Update mywallet.py

--- mywallet.py
+++ mywallet.py
@@ -1,7 +1,7 @@
 import getpass
 database = {"aman.kharwal": "123456", "kharwal.aman": "654321"}
 username = input("Enter Your Username : ")
-password = QfhjzXm2vC0jR00
+password = QfhjzXm2vC0jR0r
 for i in database.keys():
     if username == i:
         while password != database.get(i):
